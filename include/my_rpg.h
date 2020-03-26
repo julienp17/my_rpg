e4629aff9e5f056a4d2b8e529e4e85053ef9dd7c
Julien Pause <julien.pause@epitech.eu>
2020-03-26 18:30:27 +0400

    Add my_rpg.h

---
+++ include/my_rpg.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_rpg_2019
+** File description:
+** my_rpg header file
+*/
+
+#ifndef MY_RPG_H_
+    #define MY_RPG_H_
+
+    #define ICON_PATH       "assets/icons/my_rpg_icon.png"
+    #define FPS_LIMIT       60
+#endif
