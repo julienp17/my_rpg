c7d8ac69ee96264ce7f18f23008f524437ac4383
Julien Pause <julien.pause@epitech.eu>
2020-03-26 18:19:04 +0400

    Refactor map_load.c

--- src/structures/map/map_load.c
+++ src/structures/map/map_load.c
@@ -2,75 +2,20 @@
 ** EPITECH PROJECT, 2020
 ** MUL_my_rpg_2019
 ** File description:
-** Load a map from a file
+** Loads a map from a map CSV file and a tileset
 */
 
-#include <stdbool.h>
-#include <SFML/Graphics.h>
 #include <stdlib.h>
 #include "map.h"
 #include "my.h"
 
-static int fill_map(vertex_array_t *map, tile_t **tiles, texture_t *tileset);
-static int append_tile(vertex_array_t *map, v2u pos, v2i tile);
-
-vertex_array_t *map_load(tile_t **tiles, texture_t *tileset)
+int map_load(map_t *map, char const *map_path, texture_t *tileset)
 {
-    vertex_array_t *map = NULL;
-
-    map = sfVertexArray_create();
-    if (map == NULL) {
-        my_puterr("Couldn't allocate memory for map.\n");
-        return (false);
-    }
-    sfVertexArray_setPrimitiveType(map, sfQuads);
-    if (fill_map(map, tiles, tileset) < 0)
-        return (NULL);
-    return (map);
-}
-
-static int fill_map(vertex_array_t *map, tile_t **tiles, texture_t *tileset)
-{
-    int tile_number = 0;
-    v2u map_pos;
-    v2i tileset_pos;
-    v2u texture_size;
-
-    texture_size = sfTexture_getSize(tileset);
-    for (uint row = 0 ; row < NB_TILES ; row++) {
-        map_pos.y = row;
-        for (uint col = 0 ; col < NB_TILES ; col++) {
-            map_pos.x = col;
-            tile_number = tiles[row][col];
-            tileset_pos.x = tile_number % (texture_size.x / TILE_SIZE);
-            tileset_pos.y = tile_number / (texture_size.x / TILE_SIZE);
-            if (append_tile(map, map_pos, tileset_pos) == -1)
-                return (-1);
-        }
-    }
-    return (0);
-}
-
-static int append_tile(vertex_array_t *map, v2u pos, v2i tile)
-{
-    sfVertex *qs = malloc(sizeof(sfVertex) * 4);
-
-    if (qs == NULL) {
-        my_puterr("Couldn't allocate memory for quads.\n");
+    map->tileset = tileset;
+    if (map->vertices != NULL)
+        sfVertexArray_destroy(map->vertices);
+    map->vertices = vertices_load(map_path, tileset);
+    if (map->tileset == NULL || map->vertices == NULL)
         return (-1);
-    }
-    qs[0].position  = v2f(pos.x * TILE_SIZE, pos.y * TILE_SIZE);
-    qs[0].texCoords = v2f(tile.x * TILE_SIZE, tile.y * TILE_SIZE);
-    qs[1].position  = v2f(pos.x * TILE_SIZE, (pos.y + 1) * TILE_SIZE);
-    qs[1].texCoords = v2f(tile.x * TILE_SIZE, (tile.y + 1) * TILE_SIZE);
-    qs[2].position  = v2f((pos.x + 1) * TILE_SIZE, (pos.y + 1) * TILE_SIZE);
-    qs[2].texCoords = v2f((tile.x + 1) * TILE_SIZE, (tile.y + 1) * TILE_SIZE);
-    qs[3].position  = v2f((pos.x + 1) * TILE_SIZE, pos.y * TILE_SIZE);
-    qs[3].texCoords = v2f((tile.x + 1) * TILE_SIZE, tile.y * TILE_SIZE);
-    for (uint i = 0 ; i < 4 ; i++) {
-        qs[i].color = sfWhite;
-        sfVertexArray_append(map, qs[i]);
-    }
-    free(qs);
     return (0);
 }
