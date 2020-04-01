caba0a5d18285d90daaad91e9af8a93d1a576d03
Julien Pause <julien.pause@epitech.eu>
2020-04-01 18:57:02 +0400

    Add size attribute to map structure

--- include/map.h
+++ include/map.h
@@ -14,13 +14,13 @@
     #define MAP_DIR_PATH        "assets/maps/"
     #define CSV_SEP             ','
     #define TILE_SIZE           32
-    #define MAP_SIZE            1920
-    #define NB_TILES            ((MAP_SIZE) / (TILE_SIZE))
+    #define TILE_ROW_END_VALUE  -84
 
     typedef int tile_t;
     typedef sfVertexArray layer_t;
 
     typedef struct map {
+        v2f size;
         tileset_t *tileset;
         tile_t **collisions;
         layer_t *back;
@@ -38,5 +38,6 @@
     tile_t **tiles_load(char const *map_path);
     void map_destroy(map_t *map);
 
+    bool map_collided(map_t *map, v2i coords);
     void map_draw(window_t *win, map_t *map, layer_name_t layer_name);
 #endif
