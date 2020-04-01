dffd5f5988b176e8665c898b457017864d8236e0
Julien Pause <julien.pause@epitech.eu>
2020-04-01 16:20:45 +0400

    Refactor map_load completely

--- src/structures/map/map_load.c
+++ src/structures/map/map_load.c
@@ -9,13 +9,60 @@
 #include "map.h"
 #include "my.h"
 
-int map_load(map_t *map, char const *map_path, texture_t *tileset)
+static int load_tileset(map_t *map, char const *tileset_path);
+static int load_vertices(map_t *map, char const *map_name);
+static int load_collisions(map_t *map, char const *map_name);
+
+int map_load(map_t *map, char const *map_name, char const *tileset_path)
 {
-    map->tileset = tileset;
-    if (map->vertices != NULL)
-        sfVertexArray_destroy(map->vertices);
-    map->vertices = vertices_load(map_path, tileset);
-    if (map->tileset == NULL || map->vertices == NULL)
+    if (load_tileset(map, tileset_path) < 0)
         return (-1);
+    if (load_vertices(map, map_name) < 0)
+        return (-1);
+    if (load_collisions(map, map_name) < 0)
+        return (-1);
+    return (0);
+}
+
+static int load_tileset(map_t *map, char const *tileset_path)
+{
+    if (map->tileset != NULL)
+        sfTexture_destroy(map->tileset);
+    map->tileset = sfTexture_createFromFile(tileset_path, NULL);
+    if (map->tileset == NULL) {
+        my_puterr("Couldn't load map tileset.\n");
+        return (-1);
+    }
+    return (0);
+}
+
+static int load_vertices(map_t *map, char const *map_name)
+{
+    char *map_path = NULL;
+
+    if (map->back != NULL)
+        sfVertexArray_destroy(map->back);
+    if (map->top != NULL)
+        sfVertexArray_destroy(map->top);
+    map_path = my_sdup(3, MAP_DIR_PATH, map_name, "_back.csv");
+    map->back = vertices_load(map_path, map->tileset);
+    free(map_path);
+    map_path = my_sdup(3, MAP_DIR_PATH, map_name, "_top.csv");
+    map->top = vertices_load(map_path, map->tileset);
+    free(map_path);
+    if (map->back == NULL)
+        return (-1);
+    return (0);
+}
+
+static int load_collisions(map_t *map, char const *map_name)
+{
+    char *map_path = NULL;
+
+    map_path = my_sdup(3, MAP_DIR_PATH, map_name, "_collision.csv");
+    map->collisions = tiles_load(map_path);
+    if (map->collisions == NULL)
+        return (-1);
+    free(map_path);
     return (0);
 }
