25f2e87bf84b8fab5f93d7884f95777246b1b008
Julien Pause <julien.pause@epitech.eu>
2020-04-04 00:06:28 +0400

    Refactor game_poll_events

--- src/structures/game/game_poll_events.c
+++ src/structures/game/game_poll_events.c
@@ -9,7 +9,8 @@
 #include "game.h"
 #include "my_rpg.h"
 
-static void handle_keyboard_input(sfEvent *event, game_t *game);
+static bool is_movement_key(sfKeyCode key);
+static void handle_key_pressed(sfEvent *event, game_t *game);
 static void check_window_quit(sfEvent *event, sfRenderWindow *window);
 
 void game_poll_events(game_t *game)
@@ -19,7 +20,7 @@ void game_poll_events(game_t *game)
     while (sfRenderWindow_pollEvent(game->win, &event)) {
         check_window_quit(&event, game->win);
         if (event.type == sfEvtKeyPressed)
-            handle_keyboard_input(&event, game);
+            handle_key_pressed(&event, game);
     }
 }
 
@@ -27,21 +28,28 @@ static void check_window_quit(sfEvent *event, window_t *window)
 {
     if (event->type == sfEvtClosed)
         sfRenderWindow_close(window);
-    if (event->key.code == sfKeyEscape || event->key.code == sfKeyQ)
+    if (event->key.code == sfKeyEscape)
         sfRenderWindow_close(window);
 }
 
-static void handle_keyboard_input(sfEvent *event, game_t *game)
+static void handle_key_pressed(sfEvent *event, game_t *game)
 {
     switch (event->key.code) {
-    case sfKeyUp:
-    case sfKeyDown:
-    case sfKeyLeft:
-    case sfKeyRight:
+    case sfKeyUp:    game->player->orientation = BACK; break;
+    case sfKeyDown:  game->player->orientation = FRONT; break;
+    case sfKeyLeft:  game->player->orientation = LEFT; break;
+    case sfKeyRight: game->player->orientation = RIGHT; break;
+    default: break;
+    }
+    if (is_movement_key(event->key.code)) {
+        player_update_animation(game->player);
         if (player_can_move(game->player, game->map, event->key.code))
             player_move(game->player, get_offset_by_key(event->key.code));
-        break;
-    default:
-        break;
     }
+}
+
+static bool is_movement_key(sfKeyCode key)
+{
+    return (key == sfKeyUp || key == sfKeyDown
+            || key == sfKeyRight || key == sfKeyLeft);
 }
