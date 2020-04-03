e5bf07e9b8b91de8d5e36e0019c407bed01e0d95
Julien Pause <julien.pause@epitech.eu>
2020-04-03 10:34:53 +0400

    Update my_rpg.h

--- include/my_rpg.h
+++ include/my_rpg.h
@@ -9,15 +9,20 @@
     #define MY_RPG_H_
 
     #include "graphical_def.h"
+    #include "game.h"
 
     #define ICON_PATH           "assets/icons/my_rpg_icon.png"
     #define FPS_LIMIT           60
     #define W_WIDTH             1920
     #define W_HEIGHT            1080
     #define W_TITLE             "my_rpg"
+    #define VIEW_SIZE           720, 480
 
     int my_rpg(window_t *win);
     window_t *window_create(uint width, uint height, char const *title);
 
+    void update_view(view_t *view, player_t *player, map_t *map);
+
     v2f get_offset_by_key(sfKeyCode key);
+    v2f sprite_get_center(sprite_t *sprite);
 #endif
