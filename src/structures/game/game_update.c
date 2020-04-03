7891f289c834ffbd494e3a52d0ecf7a837f5b0f6
Julien Pause <julien.pause@epitech.eu>
2020-04-04 00:10:09 +0400

    Add game_update.c

---
+++ src/structures/game/game_update.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_rpg_2019
+** File description:
+** game_update
+*/
+
+#include "game.h"
+#include "my_rpg.h"
+
+void game_update(game_t *game, int delta_time)
+{
+    player_update(game->player, delta_time);
+    game_update_view(game);
+}
