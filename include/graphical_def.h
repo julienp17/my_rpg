6ffca732af2ea56ff7f0e8a032bb05ac435ba33e
Julien Pause <julien.pause@epitech.eu>
2020-04-03 23:57:26 +0400

    Add typedef for sfIntRect -> irect and sfFloatRect -> frect

--- include/graphical_def.h
+++ include/graphical_def.h
@@ -19,6 +19,8 @@
     typedef sfVector2u                      v2u;
     typedef sfVector2f                      v2f;
     typedef sfVector3f                      v3f;
+    typedef sfIntRect                       irect;
+    typedef sfFloatRect                     frect;
     typedef sfVideoMode                     video_mode;
     typedef sfFont                          font_t;
     typedef sfMusic                         music_t;
@@ -30,10 +32,13 @@
     typedef sfEvent                         event_t;
     typedef sfSprite                        sprite_t;
     typedef sfView                          view_t;
+    typedef sfClock                         gclock_t;
 
-    #define v2i(x, y)           ((v2i) {(x), (y)})
-    #define v2u(x, y)           ((v2u) {(x), (y)})
-    #define v2f(x, y)           ((v2f) {(x), (y)})
-    #define v3f(x, y)           ((v3f) {(x), (y)})
-    #define RGBA(r, g, b, a)    ((sfColor_fromRGBA(r, g, b, a)))
+    #define v2i(x, y)                       ((v2i) {(x), (y)})
+    #define v2u(x, y)                       ((v2u) {(x), (y)})
+    #define v2f(x, y)                       ((v2f) {(x), (y)})
+    #define v3f(x, y)                       ((v3f) {(x), (y)})
+    #define irect(x, y, w, h)               ((irect){(x), (y), (w), (h)})
+    #define frect(x, y, w, h)               ((frect){(x), (y), (w), (h)})
+    #define RGBA(r, g, b, a)                ((sfColor_fromRGBA(r, g, b, a)))
 #endif
