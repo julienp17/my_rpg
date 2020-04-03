53d99f071e762d2c2e7eac1ae66cd8af9614de7e
Julien Pause <julien.pause@epitech.eu>
2020-04-03 23:59:58 +0400

    Add animation_t and orientation_t fields to player structure

--- include/player.h
+++ include/player.h
@@ -9,14 +9,26 @@
     #define PLAYER_H_
 
     #include "graphical_def.h"
+    #include "animation.h"
     #include "map.h"
 
     #define PLAYER_TILE_SIZE        32
     #define PLAYER_TILESET_PATH     "assets/tilesets/player.png"
+    #define PLAYER_TILESET_ROWS     4
+    #define PLAYER_TILESET_COLS     3
+
+    typedef enum orientation {
+        FRONT,
+        LEFT,
+        RIGHT,
+        BACK
+    } orientation_t;
 
     typedef struct player {
+        animation_t *anim;
         sprite_t *sprite;
         texture_t *tileset;
+        orientation_t orientation;
     } player_t;
 
     player_t *player_create(void);
@@ -26,4 +38,6 @@
     void player_draw(window_t *win, player_t *player);
     bool player_can_move(player_t *player, map_t *map, sfKeyCode key);
     void player_move(player_t *player, v2f offset);
+    void player_update(player_t *player, int delta_time);
+    void player_update_animation(player_t *player);
 #endif
