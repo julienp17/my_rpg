fca8ed367d46e4e5f518ec025f18b68b02bd24dc
Julien Pause <julien.pause@epitech.eu>
2020-04-04 00:28:32 +0400

    Refactor my_rpg.c

--- src/my_rpg.c
+++ src/my_rpg.c
@@ -8,6 +8,8 @@
 #include "game.h"
 #include "my_rpg.h"
 
+static void game_loop(game_t *game);
+
 int my_rpg(window_t *win)
 {
     game_t *game = NULL;
@@ -17,11 +19,17 @@ int my_rpg(window_t *win)
         return (-1);
     if (game_load(game) == -1)
         return (-1);
-    while (sfRenderWindow_isOpen(game->win)) {
-        game_poll_events(game);
-        update_view(game->view, game->player, game->map);
-        sfRenderWindow_setView(game->win, game->view);
-        game_draw(game);
-    }
+    while (sfRenderWindow_isOpen(game->win))
+        game_loop(game);
     return (0);
+}
+
+static void game_loop(game_t *game)
+{
+    int delta_time = 0;
+
+    game_poll_events(game);
+    delta_time = sfTime_asMilliseconds(sfClock_restart(game->clock));
+    game_update(game, delta_time);
+    game_draw(game);
 }
