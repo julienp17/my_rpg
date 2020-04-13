77e3296d8a601a9017d880d0ae0e13d1c4139aad
Julien Pause <julien.pause@epitech.eu>
2020-04-13 14:50:23 +0400

    Move debug_map to its own directory

--- assets/maps/debug/map1.tsx
+++ assets/maps/debug/map1.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.3.3" name="map1" tilewidth="32" tileheight="32" tilecount="400" columns="10">
- <image source="../tilesets/map1.png" width="320" height="1280"/>
+ <image source="../../tilesets/map1.png" width="320" height="1280"/>
 </tileset>
