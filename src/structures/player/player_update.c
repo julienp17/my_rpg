b2def16b8ac9aaacec5dd4c43f9df0c03aae1677
Julien Pause <julien.pause@epitech.eu>
2020-04-16 09:54:49 +0400

    Refactor player_update

--- src/structures/player/player_update.c
+++ src/structures/player/player_update.c
@@ -10,8 +10,8 @@
 void player_update(player_t *player, int delta_time)
 {
     player->anim->elapsed += delta_time;
-    if (animation_can_update(player->anim) == false)
-        return;
-    player->anim->elapsed -= player->anim->switch_time;
-    player_update_animation(player);
+    if (animation_can_update(player->anim)) {
+        player_update_animation(player);
+        player->anim->elapsed -= player->anim->switch_time;
+    }
 }
