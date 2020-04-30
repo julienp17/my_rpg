3287e58692e0521d71e244b17281d530f4b62a1d
Julien Pause <julien.pause@epitech.eu>
2020-04-30 10:05:52 +0400

    Refactor player_update

--- src/structures/player/player_update.c
+++ src/structures/player/player_update.c
@@ -6,8 +6,13 @@
 */
 
 #include "player.h"
+#include "my_rpg.h"
 
-void player_update(player_t *player, int delta_time)
+void player_update(player_t *player, map_t *map, int delta_time)
 {
     animation_update(player, (void *)&player_update_animation, delta_time);
+    if (player_can_move(player, map)) {
+        player_move(player);
+        game_handle_warps(map, player);
+    }
 }
