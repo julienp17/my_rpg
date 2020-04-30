8e9fa83d31d88a4ca4361c26e706d93d4224815e
Julien Pause <julien.pause@epitech.eu>
2020-04-30 09:49:51 +0400

    Rename get_offset_by_key to get_dir_by_key

--- src/utilities/get_dir_by_key.c
+++ src/utilities/get_dir_by_key.c
@@ -2,19 +2,19 @@
 ** EPITECH PROJECT, 2020
 ** MUL_my_rpg_2019
 ** File description:
-** get_offset_by_key
+** get_dir_by_key
 */
 
 #include "graphical_def.h"
 
-v2f get_offset_by_key(sfKeyCode key)
+v2f get_dir_by_key(sfKeyCode key)
 {
-    v2f offset;
+    v2f dir = v2f(0.0, 0.0);
 
-    offset = (key == sfKeyUp)   ? v2f(0, -1) :
+    dir =   (key == sfKeyUp)    ? v2f(0, -1) :
             (key == sfKeyDown)  ? v2f(0, 1)  :
             (key == sfKeyLeft)  ? v2f(-1, 0) :
             (key == sfKeyRight) ? v2f(1, 0)  :
             v2f(0, 0);
-    return (offset);
+    return (dir);
 }
