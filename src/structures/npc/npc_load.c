a7560b3266d2d5930a41a539a18eb3057e6f4266
Julien Pause <julien.pause@epitech.eu>
2020-04-30 15:58:52 +0400

    Fill npc text in npc_load

--- src/structures/npc/npc_load.c
+++ src/structures/npc/npc_load.c
@@ -5,10 +5,10 @@
 ** npc_load_from_file
 */
 
+#include <stdlib.h>
 #include "file_reading.h"
 #include "npc.h"
 #include "my.h"
-
 #include "map.h"
 
 int npc_load(npc_t *npc, char const *pathname, tileset_t *npc_tileset)
@@ -22,9 +22,11 @@ int npc_load(npc_t *npc, char const *pathname, tileset_t *npc_tileset)
         return (-1);
     npc->name = my_strdup(file_lines[0]);
     pos = v2f_from_str(file_lines[1], ' ');
-    pos.x *= TILE_SIZE;
-    pos.y *= TILE_SIZE;
+    pos = v2f_multiply(pos, v2f(TILE_SIZE, TILE_SIZE));
     sprite_rect = irect_from_str(file_lines[2], ' ');
+    npc->text = my_strarr_dup(file_lines + 3);
+    if (npc->text == NULL)
+        return (-1);
     sfSprite_setPosition(npc->sprite, pos);
     sfSprite_setTexture(npc->sprite, npc_tileset, sfFalse);
     sfSprite_setTextureRect(npc->sprite, sprite_rect);
