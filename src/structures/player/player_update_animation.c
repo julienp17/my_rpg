50e8ce87a1ba52def2521984ba728a0d3a5afe80
Julien Pause <julien.pause@epitech.eu>
2020-04-30 10:03:15 +0400

    Refactor player_update_animation

--- src/structures/player/player_update_animation.c
+++ src/structures/player/player_update_animation.c
@@ -9,15 +9,17 @@
 
 void player_update_animation(player_t *player)
 {
-    v2u tileset_size;
     irect sprite_rect;
 
-    tileset_size = sfTexture_getSize(player->tileset);
     sprite_rect = sfSprite_getTextureRect(player->sprite);
-    sprite_rect.top = sprite_rect.height * player->orientation;
-    if (sprite_rect.left + sprite_rect.width >= (int)tileset_size.x)
+    sprite_rect.top = sprite_rect.height * (player->orientation % sfKeyLeft);
+    if (player->move_speed == 0) {
         sprite_rect.left = 0;
-    else
-        sprite_rect.left += sprite_rect.width;
+    } else {
+        if (sprite_rect.left == sprite_rect.width)
+            sprite_rect.left += sprite_rect.width;
+        else
+            sprite_rect.left = sprite_rect.width;
+    }
     sfSprite_setTextureRect(player->sprite, sprite_rect);
 }
