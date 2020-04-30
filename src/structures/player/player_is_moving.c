74def9632175f8a11a19906a1cc0293219b6fc5b
Julien Pause <julien.pause@epitech.eu>
2020-04-30 17:18:08 +0400

    Add player_is_moving.c

---
+++ src/structures/player/player_is_moving.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** MUL_my_rpg_2019
+** File description:
+** Returns true if the player is moving, false otherwise
+*/
+
+#include "player.h"
+
+bool player_is_moving(player_t *player)
+{
+    return (player->move_speed != 0);
+}
