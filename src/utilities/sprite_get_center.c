976ef4fd58a860e57c1c22eaf208f22fc608ad60
Julien Pause <julien.pause@epitech.eu>
2020-04-30 18:57:23 +0400

    Refactor sprite_get_center

--- src/utilities/sprite_get_center.c
+++ src/utilities/sprite_get_center.c
@@ -9,10 +9,8 @@
 
 v2f sprite_get_center(sprite_t *sprite)
 {
-    v2f pos;
     sfFloatRect bounds;
 
-    pos = sfSprite_getPosition(sprite);
     bounds = sfSprite_getGlobalBounds(sprite);
-    return (v2f(pos.x + bounds.width / 2, pos.y + bounds.height / 2));
+    return (v2f(bounds.left + bounds.width / 2,bounds.top + bounds.height / 2));
 }
