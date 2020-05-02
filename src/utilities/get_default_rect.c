cea6ad21917d117c3babbbb3cb92accbef62ac6a
Julien Pause <julien.pause@epitech.eu>
2020-05-02 20:51:30 +0400

    Refactor get_default_rect

--- src/utilities/get_default_rect.c
+++ src/utilities/get_default_rect.c
@@ -7,13 +7,17 @@
 
 #include "graphical_def.h"
 
-rect_t *get_default_rect(v2f pos, v2f size)
+rect_t *get_default_rect(frect bounds)
 {
     rect_t *rect = NULL;
+    v2f pos = v2f(0.0f, 0.0f);
+    v2f size = v2f(0.0f, 0.0f);
 
     rect = sfRectangleShape_create();
     if (rect == NULL)
         return (NULL);
+    pos = v2f(bounds.left, bounds.top);
+    size = v2f(bounds.width, bounds.height);
     sfRectangleShape_setPosition(rect, pos);
     sfRectangleShape_setSize(rect, size);
     sfRectangleShape_setFillColor(rect, TRANSPARENT_GREY);
